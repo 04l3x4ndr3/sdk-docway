ed695c5fcdc6ce488b353c5ee738917608e99183
04l3x4ndr3 <alexandre@2plug.com.br>
2023-01-18 17:39:27 -0300

    Ajustes e inclusão de arquivo de Constants

--- src/Helpers/CallApi.php
+++ src/Helpers/CallApi.php
@@ -48,7 +48,7 @@ class CallApi {
 	 * @return object
 	 * @throws GuzzleException
 	 */
-	public function call(string $method, string $endpoint, ?array $body = NULL): ?array
+	public function call(string $method, string $endpoint, ?array $body = NULL): array | object
 	{
 		$token = $this->accessToken();
 		$client = new Client();
@@ -60,7 +60,7 @@ class CallApi {
 			'json' => $body
 		]);
 		$res = $client->request($method, "{$this->config->getUrl()}{$endpoint}", $options);
-		return json_decode($res->getBody(), true);
+		return json_decode($res->getBody());
 	}
 
 	/**
