65fbfd28462f5ca879175746e8b20a67c95dec46
04l3x4ndr3 <alexandre@2plug.com.br>
2024-09-06 14:13:56 -0300

    Reorder constants for clarity
    Moved DOCTOR_VIDEO_NOSHOW constant to be in sequence with other appointment status constants. This change improves the readability and organization of the constants in the file.

--- src/Helpers/Constants.php
+++ src/Helpers/Constants.php
@@ -1,15 +1,14 @@
 <?php
+
 namespace O4l3x4ndr3\SdkDocway\Constants;
 
 /**
  * appointment status
  */
 const  APPOINTMENT_CREATED = 1;
-
-const DOCTOR_VIDEO_NOSHOW = 99;
-
 const APPOINTMENT_ACCEPTED = 6;
 const APPOINTMENT_FINISHED = 7;
 const APPOINTMENT_CANCELED = 8;
 const APPOINTMENT_CANCELED_PATIENT = 10;
 const APPOINTMENT_CANCELED_DOCTOR = 11;
+const DOCTOR_VIDEO_NOSHOW = 12;
